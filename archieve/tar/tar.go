7f5e9e9c1881e6fcf3481e2da1d08e11cb8a9599
GreatFeng <clbnclbn@163.com>
2016-01-08 00:29:29 +0800

    压缩文件和归档文件的读写

--- archieve/tar/tar.go
+++ archieve/tar/tar.go
@@ -6,40 +6,51 @@
 
 // Package tar implements access to tar archives. It aims to cover most of the
 // variations, including those produced by GNU and BSD tars.
-//
-// References:
-//
-//	http://www.freebsd.org/cgi/man.cgi?query=tar&sektion=5
-//	http://www.gnu.org/software/tar/manual/html_node/Standard.html
-//	http://pubs.opengroup.org/onlinepubs/9699919799/utilities/pax.html
 
-// tar包实现了tar格式压缩文件的存取.
-// 本包目标是覆盖大多数tar的变种，包括GNU和BSD生成的tar文件。
-//
-// 参见：
+// tar 包实现了 tar 格式压缩文件的存取. 本包目标是覆盖大多数 tar 的变种, 包括
+// GNU 和 BSD 生成的 tar 文件.
+
+// References:
 //
 //	http://www.freebsd.org/cgi/man.cgi?query=tar&sektion=5
 //	http://www.gnu.org/software/tar/manual/html_node/Standard.html
 //	http://pubs.opengroup.org/onlinepubs/9699919799/utilities/pax.html
 package tar
 
-const (
+//const (
+//	// Types
+//	TypeReg           = '0'    // regular file
+//	TypeRegA          = '\x00' // regular file
+//	TypeLink          = '1'    // hard link
+//	TypeSymlink       = '2'    // symbolic link
+//	TypeChar          = '3'    // character device node
+//	TypeBlock         = '4'    // block device node
+//	TypeDir           = '5'    // directory
+//	TypeFifo          = '6'    // fifo node
+//	TypeCont          = '7'    // reserved
+//	TypeXHeader       = 'x'    // extended header
+//	TypeXGlobalHeader = 'g'    // global extended header
+//	TypeGNULongName   = 'L'    // Next file has a long name
+//	TypeGNULongLink   = 'K'    // Next file symlinks to a file w/ a long name
+//	TypeGNUSparse     = 'S'    // sparse file
+//)
 
-	// Types
-	TypeReg           = '0'    // regular file
-	TypeRegA          = '\x00' // regular file
-	TypeLink          = '1'    // hard link
-	TypeSymlink       = '2'    // symbolic link
-	TypeChar          = '3'    // character device node
-	TypeBlock         = '4'    // block device node
-	TypeDir           = '5'    // directory
-	TypeFifo          = '6'    // fifo node
-	TypeCont          = '7'    // reserved
-	TypeXHeader       = 'x'    // extended header
-	TypeXGlobalHeader = 'g'    // global extended header
-	TypeGNULongName   = 'L'    // Next file has a long name
-	TypeGNULongLink   = 'K'    // Next file symlinks to a file w/ a long name
-	TypeGNUSparse     = 'S'    // sparse file
+const (
+	// 文件类型
+	TypeReg           = '0'    // 常规文件
+	TypeRegA          = '\x00' // 常规文件
+	TypeLink          = '1'    // 硬链接
+	TypeSymlink       = '2'    // 符号链接
+	TypeChar          = '3'    // 字符设备节点
+	TypeBlock         = '4'    // 块设备节点
+	TypeDir           = '5'    // 目录
+	TypeFifo          = '6'    // 先入先出节点
+	TypeCont          = '7'    // 保留
+	TypeXHeader       = 'x'    // 扩展头部
+	TypeXGlobalHeader = 'g'    // 全局扩展头部
+	TypeGNULongName   = 'L'    // 下一个文件文件名很长
+	TypeGNULongLink   = 'K'    // 下一个符号链接链接到的文件名称很长
+	TypeGNUSparse     = 'S'    // 稀疏文件
 )
 
 var (
@@ -54,23 +65,40 @@ var (
 
 // A Header represents a single header in a tar archive. Some fields may not be
 // populated.
+//type Header struct {
+//	Name       string    // name of header file entry
+//	Mode       int64     // permission and mode bits
+//	Uid        int       // user id of owner
+//	Gid        int       // group id of owner
+//	Size       int64     // length in bytes
+//	ModTime    time.Time // modified time
+//	Typeflag   byte      // type of header entry
+//	Linkname   string    // target name of link
+//	Uname      string    // user name of owner
+//	Gname      string    // group name of owner
+//	Devmajor   int64     // major number of character or block device
+//	Devminor   int64     // minor number of character or block device
+//	AccessTime time.Time // access time
+//	ChangeTime time.Time // status change time
+//	Xattrs     map[string]string
+//}
 
-// Header代表tar档案文件里的单个头。 Header类型的某些字段可能未使用。
+// Header 代表 tar 档案文件里的单个头. 某些字段可能未被填充使用.
 type Header struct {
-	Name       string    // name of header file entry
-	Mode       int64     // permission and mode bits
-	Uid        int       // user id of owner
-	Gid        int       // group id of owner
-	Size       int64     // length in bytes
-	ModTime    time.Time // modified time
-	Typeflag   byte      // type of header entry
-	Linkname   string    // target name of link
-	Uname      string    // user name of owner
-	Gname      string    // group name of owner
-	Devmajor   int64     // major number of character or block device
-	Devminor   int64     // minor number of character or block device
-	AccessTime time.Time // access time
-	ChangeTime time.Time // status change time
+	Name       string    // 头部名称, 一般设置为文件名全路径
+	Mode       int64     // 权限和模式位
+	Uid        int       // 用户id
+	Gid        int       // 用户组id
+	Size       int64     // 按字节表示长度
+	ModTime    time.Time // 修改时间
+	Typeflag   byte      // 头部条目类型
+	Linkname   string    // 链接的目标名称
+	Uname      string    // 用户名
+	Gname      string    // 用户组名
+	Devmajor   int64     // 字符或块主设备号
+	Devminor   int64     // 字符或块次设备号
+	AccessTime time.Time // 访问时间
+	ChangeTime time.Time // 状态改变时间
 	Xattrs     map[string]string
 }
 
@@ -81,16 +109,15 @@ type Header struct {
 // modify the Name field of the returned header to provide the full path name of
 // the file.
 
-// FileInfoHeader返回一个根据fi填写了部分字段的Header。
-// 如果fi描述一个符号链接，FileInfoHeader函数将link参数作为链接目标。
-// 如果fi描述一个目录，会在名字后面添加斜杠。
-// 因为os.FileInfo接口的Name方法只返回它描述的文件的无路径名，
-// 有可能需要将返回值的Name字段修改为文件的完整路径名。
+// FileInfoHeader 创建一个根据 fi 填写了部分字段的 Header. 如果 fi 描述一个符号链接,
+// FileInfoHeader 将 link 参数作为链接目标. 如果 fi 描述一个目录, 则会在名字后面添加
+// 斜杠. 因为 os.FileInfo 接口的 Name 方法只返回它描述的文件的无路径名, 有可能需要将
+// 返回值的 Name 字段修改为文件的完整路径名.
 func FileInfoHeader(fi os.FileInfo, link string) (*Header, error)
 
 // FileInfo returns an os.FileInfo for the Header.
 
-// FileInfo返回该Header对应的文件信息。（os.FileInfo类型）
+// FileInfo 返回 Header 对应的 os.FileInfo.
 func (h *Header) FileInfo() os.FileInfo
 
 // A Reader provides sequential access to the contents of a tar archive. A tar
@@ -98,32 +125,29 @@ func (h *Header) FileInfo() os.FileInfo
 // file in the archive (including the first), and then it can be treated as an
 // io.Reader to access the file's data.
 
-// Reader提供了对一个tar档案文件的顺序读取。
-// 一个tar档案文件包含一系列文件。
-// Next方法返回档案中的下一个文件（包括第一个），
-// 返回值可以被视为io.Reader来获取文件的数据。
+// Reader 提供了对一个 tar 档案文件的顺序读取. 一个 tar 档案文件包含一系列文件.
+// Next 方法前进到档案中的下一个文件(包括第一个), 返回值可以被视为一个 io.Reader
+// 来获取文件的数据.
 type Reader struct {
 	// contains filtered or unexported fields
 }
 
 // NewReader creates a new Reader reading from r.
 
-// NewReader创建一个从r读取的Reader。
+// NewReader 创建一个从 r 读取数据的 Reader.
 func NewReader(r io.Reader) *Reader
 
 // Next advances to the next entry in the tar archive.
 
-// Next 将前进到 tar 归档文件中的下一条记录，
-//
-// 在输入到达结尾时将返回 io.EOF。
+// Next 将前进到 tar 归档文件中的下一条记录.
 func (tr *Reader) Next() (*Header, error)
 
 // Read reads from the current entry in the tar archive. It returns 0, io.EOF when
 // it reaches the end of that entry, until Next is called to advance to the next
 // entry.
 
-// 从档案文件的当前记录读取数据， 到达记录末端时返回(0,
-// EOF)，直到调用Next方法转入下一记录。
+// 从档案文件的当前记录读取数据, 到达记录末端时返回(0, io.EOF), 直到调用 Next 方法
+// 前进到下一记录.
 func (tr *Reader) Read(b []byte) (n int, err error)
 
 // A Writer provides sequential writing of a tar archive in POSIX.1 format. A tar
@@ -131,42 +155,40 @@ func (tr *Reader) Read(b []byte) (n int, err error)
 // and then call Write to supply that file's data, writing at most hdr.Size bytes
 // in total.
 
-// Writer类型提供了POSIX.1格式的tar档案文件的顺序写入。
-// 一个tar档案文件包含一系列文件。 调用WriteHeader来写入一个新的文件，
-// 然后调用Write写入文件的数据，该记录写入的数据不能超过hdr.Size字节。
+// Writer 类型提供了 POSIX.1 格式的 tar 档案文件的顺序写入. 一个 tar 档案文件包含一
+// 系列文件. 调用 WriteHeader 来创建一个新的文件, 然后调用 Write 写入文件的数据, 该
+// 记录写入的数据不能超过 hdr.Size 字节.
 type Writer struct {
 	// contains filtered or unexported fields
 }
 
 // NewWriter creates a new Writer writing to w.
 
-// NewWriter创建一个写入w的*Writer。
+// NewWriter 创建一个写入数据到 w 的 Writer.
 func NewWriter(w io.Writer) *Writer
 
 // Close closes the tar archive, flushing any unwritten data to the underlying
 // writer.
 
-// Close关闭tar档案文件，
-// 会将缓冲中未写入下层的io.Writer接口的数据刷新到下层。
+// Close 关闭 tar 档案文件, 并将缓冲中未写入下层 io.Writer 接口的数据刷新到下层.
 func (tw *Writer) Close() error
 
 // Flush finishes writing the current file (optional).
 
-// Flush结束当前文件的写入。（可选的）
+// Flush 结束当前文件的写入(可选的).
 func (tw *Writer) Flush() error
 
 // Write writes to the current entry in the tar archive. Write returns the error
 // ErrWriteTooLong if more than hdr.Size bytes are written after WriteHeader.
 
-// Write向tar档案文件的当前记录中写入数据。
-// 如果写入的数据总数超出上一次调用WriteHeader的参数hdr.Size字节， 返回ErrWriteTooLong错误。
+// Write 向 tar 档案文件的当前记录中写入数据. 如果写入的数据总长度超出上一次调用
+// WriteHeader 时的参数 hdr.Size, 会返回 ErrWriteTooLong 错误.
 func (tw *Writer) Write(b []byte) (n int, err error)
 
 // WriteHeader writes hdr and prepares to accept the file's contents. WriteHeader
 // calls Flush if it is not the first header. Calling after a Close will return
 // ErrWriteAfterClose.
 
-// WriteHeader写入hdr并准备接受文件内容。
-// 如果不是第一次调用本方法，会调用Flush。
-// 在Close之后调用本方法会返回ErrWriteAfterClose。
+// WriteHeader 写入 hdr 并准备接受文件内容. 如果不是第一次调用本方法, 会调用 Flush.
+// 在 Close 之后调用本方法会返回 ErrWriteAfterClose.
 func (tw *Writer) WriteHeader(hdr *Header) error
--- archieve/zip/zip.go
+++ archieve/zip/zip.go
@@ -5,11 +5,15 @@
 // +build ingore
 
 // Package zip provides support for reading and writing ZIP archives.
-//
+
+// zip 包提供了 zip 档案文件的读写服务.
+
 // See: http://www.pkware.com/documents/casestudies/APPNOTE.TXT
 //
 // This package does not support disk spanning.
-//
+
+// 本包不支持跨硬盘的压缩.
+
 // A note about ZIP64:
 //
 // To be backwards compatible the FileHeader has both 32 and 64 bit Size fields.
@@ -17,22 +21,16 @@
 // both fields will be the same. For files requiring the ZIP64 format the 32 bit
 // fields will be 0xffffffff and the 64 bit fields must be used instead.
 
-// zip包提供了zip档案文件的读写服务.
-//
-// 参见http://www.pkware.com/documents/casestudies/APPNOTE.TXT
-//
-// 本包不支持跨硬盘的压缩。
-//
 // 关于ZIP64：
 //
-// 为了向下兼容，FileHeader同时拥有32位和64位的Size字段。
-// 64位字段总是包含正确的值，对普通格式的档案未见它们的值是相同的。
-// 对zip64格式的档案文件32位字段将是0xffffffff，必须使用64位字段。
+// 为了向下兼容, FileHeader 同时拥有 32 位和 64 位的 Size 字段. 64 位字段总是包含
+// 正确的值, 对普通格式的档案来说它们的值是相同的. 对 zip64 格式的档案文件 32 位字
+// 段将是 0xffffffff, 必须使用64位字段.
 package zip
 
 // Compression methods.
 
-// 预定义压缩算法。
+// 预定义压缩算法.
 const (
 	Store   uint16 = 0
 	Deflate uint16 = 8
@@ -47,20 +45,20 @@ var (
 // RegisterCompressor registers custom compressors for a specified method ID. The
 // common methods Store and Deflate are built in.
 
-// RegisterCompressor使用指定的方法ID注册一个Compressor类型函数。
-// 常用的方法Store和Deflate是内建的。
+// RegisterCompressor 使用指定的方法 ID 注册一个 Compressor 类型函数. 常用的方法
+// Store 和 Deflate 是内建的.
 func RegisterCompressor(method uint16, comp Compressor)
 
 // RegisterDecompressor allows custom decompressors for a specified method ID.
 
-// RegisterDecompressor使用指定的方法ID注册一个Decompressor类型函数。
+// RegisterDecompressor 使用指定的方法 ID 注册一个 Decompressor 类型函数.
 func RegisterDecompressor(method uint16, d Decompressor)
 
 // A Compressor returns a compressing writer, writing to the provided writer. On
 // Close, any pending data should be flushed.
 
-// Compressor函数类型会返回一个io.WriteCloser，该接口会将数据压缩后写入提供的接口。
-// 关闭时，应将缓冲中的数据刷新到下层接口中。
+// Compressor 函数类型会返回一个 io.WriteCloser, 该接口会将数据压缩后写入提供的
+// io.Writer. 关闭时, 应将缓冲中的数据刷新到下层接口中.
 type Compressor func(io.Writer) (io.WriteCloser, error)
 
 // Decompressor is a function that wraps a Reader with a decompressing Reader. The
@@ -68,10 +66,9 @@ type Compressor func(io.Writer) (io.WriteCloser, error)
 // archive. These callers are responsible for closing this reader when they're
 // finished reading.
 
-// Decompressor函数类型会把一个io.Reader包装成具有decompressing特性的io.Reader.
-// Decompressor函数类型会返回一个io.ReadCloser，
-// 该接口的Read方法会将读取自提供的接口的数据提前解压缩。
-// 程序员有责任在读取结束时关闭该io.ReadCloser。
+// Decompressor 函数类型会把一个 io.Reader 包装成具有解压缩功能的 io.ReadCloser.
+// io.ReadCloser 被返回给打开档案内文件的调用者. 这些调用者有责任在读取结束时关闭
+// 该 io.ReadCloser.
 type Decompressor func(io.Reader) io.ReadCloser
 
 type File struct {
@@ -85,40 +82,59 @@ type File struct {
 // Most callers should instead use Open, which transparently decompresses data and
 // verifies checksums.
 
-// DataOffset返回文件的可能存在的压缩数据相对于zip文件起始的偏移量。
-// 大多数调用者应使用Open代替，该方法会主动解压缩数据并验证校验和。
+// DataOffset 返回文件中可能存在的压缩数据相对于 zip 文件起始处的偏移量. 大多数调用
+// 者应使用 Open 代替, 该方法会主动解压缩数据并验证校验和.
 func (f *File) DataOffset() (offset int64, err error)
 
 // Open returns a ReadCloser that provides access to the File's contents. Multiple
 // files may be read concurrently.
 
-// Open方法返回一个io.ReadCloser接口，提供读取文件内容的方法。
-// 可以同时读取多个文件。
+// Open 方法返回一个 io.ReadCloser 接口, 提供读取文件内容的方法. 可以同时读取多个文件.
 func (f *File) Open() (rc io.ReadCloser, err error)
 
 // FileHeader describes a file within a zip file. See the zip spec for details.
 
-// FileHeader描述zip文件中的一个文件。 参见zip的定义获取细节。
+// FileHeader 描述 zip 文件中的一个文件. 参见zip的定义获取细节.
+//type FileHeader struct {
+//	// Name is the name of the file.
+//	// It must be a relative path: it must not start with a drive
+//	// letter (e.g. C:) or leading slash, and only forward slashes
+//	// are allowed.
+//	Name string
+//
+//	CreatorVersion     uint16
+//	ReaderVersion      uint16
+//	Flags              uint16
+//	Method             uint16
+//	ModifiedTime       uint16 // MS-DOS time
+//	ModifiedDate       uint16 // MS-DOS date
+//	CRC32              uint32
+//	CompressedSize     uint32 // deprecated; use CompressedSize64
+//	UncompressedSize   uint32 // deprecated; use UncompressedSize64
+//	CompressedSize64   uint64
+//	UncompressedSize64 uint64
+//	Extra              []byte
+//	ExternalAttrs      uint32 // Meaning depends on CreatorVersion
+//	Comment            string
+//}
 type FileHeader struct {
-	// Name is the name of the file.
-	// It must be a relative path: it must not start with a drive
-	// letter (e.g. C:) or leading slash, and only forward slashes
-	// are allowed.
+	// Name 是这个文件的名称
+	// 该名称必须是相对路径，不可以以盘符(比如C:)或者反斜杠(\)开头。允许正斜杠(/)存在。
 	Name string
 
 	CreatorVersion     uint16
 	ReaderVersion      uint16
 	Flags              uint16
 	Method             uint16
-	ModifiedTime       uint16 // MS-DOS time
-	ModifiedDate       uint16 // MS-DOS date
+	ModifiedTime       uint16 // MS-DOS 时间
+	ModifiedDate       uint16 // MS-DOS 日期
 	CRC32              uint32
-	CompressedSize     uint32 // deprecated; use CompressedSize64
-	UncompressedSize   uint32 // deprecated; use UncompressedSize64
+	CompressedSize     uint32 // 已废弃; 使用 CompressedSize64
+	UncompressedSize   uint32 // 已废弃; 使用 UncompressedSize64
 	CompressedSize64   uint64
 	UncompressedSize64 uint64
 	Extra              []byte
-	ExternalAttrs      uint32 // Meaning depends on CreatorVersion
+	ExternalAttrs      uint32 // 参数含义依赖于CreatorVersion
 	Comment            string
 }
 
@@ -127,35 +143,35 @@ type FileHeader struct {
 // describes, it may be necessary to modify the Name field of the returned header
 // to provide the full path name of the file.
 
-// FileInfoHeader返回一个根据fi填写了部分字段的Header。
-// 因为os.FileInfo接口的Name方法只返回它描述的文件的无路径名，
-// 有可能需要将返回值的Name字段修改为文件的完整路径名。
+// FileInfoHeader 创建一个根据 fi 填写了部分字段的 FileHeader. 因为 os.FileInfo
+// 接口的 Name 方法只返回它描述的文件的无路径名, 有可能需要将返回值的 Name 字段修
+// 改为文件的完整路径名.
 func FileInfoHeader(fi os.FileInfo) (*FileHeader, error)
 
 // FileInfo returns an os.FileInfo for the FileHeader.
 
-// FileInfo返回一个根据h的信息生成的os.FileInfo。
+// FileInfo 返回一个根据 h 的信息生成的 os.FileInfo.
 func (h *FileHeader) FileInfo() os.FileInfo
 
 // ModTime returns the modification time in UTC. The resolution is 2s.
 
-// 返回最近一次修改的UTC时间。（精度2s）
+// ModTime 返回最近一次修改的 UTC 时间. 精度2s.
 func (h *FileHeader) ModTime() time.Time
 
 // Mode returns the permission and mode bits for the FileHeader.
 
-// Mode返回h的权限和模式位。
+// Mode 返回 h 的权限和模式位.
 func (h *FileHeader) Mode() (mode os.FileMode)
 
 // SetModTime sets the ModifiedTime and ModifiedDate fields to the given time in
 // UTC. The resolution is 2s.
 
-// 将ModifiedTime和ModifiedDate字段设置为给定的UTC时间。（精度2s）
+// SetModTime 将 ModifiedTime 和 ModifiedDate 字段设置为给定的 UTC 时间. 精度2s.
 func (h *FileHeader) SetModTime(t time.Time)
 
 // SetMode changes the permission and mode bits for the FileHeader.
 
-// SetMode修改h的权限和模式位。
+// SetMode 修改 h 的权限和模式位.
 func (h *FileHeader) SetMode(mode os.FileMode)
 
 type ReadCloser struct {
@@ -165,12 +181,12 @@ type ReadCloser struct {
 
 // OpenReader will open the Zip file specified by name and return a ReadCloser.
 
-// OpenReader会打开name指定的zip文件并返回一个*ReadCloser。
+// OpenReader 会打开 name 指定的 zip 文件并返回一个 ReadCloser.
 func OpenReader(name string) (*ReadCloser, error)
 
 // Close closes the Zip file, rendering it unusable for I/O.
 
-// Close关闭zip文件，使它不能用于I/O。
+// Close 关闭 zip 文件, 使它不能用于 I/O.
 func (rc *ReadCloser) Close() error
 
 type Reader struct {
@@ -182,26 +198,25 @@ type Reader struct {
 // NewReader returns a new Reader reading from r, which is assumed to have the
 // given size in bytes.
 
-// NewReader返回一个从r读取数据的*Reader，r被假设其大小为size字节。
+// NewReader 创建并返回一个从 r 读取数据的 Reader, r 被假设其大小为 size 字节.
 func NewReader(r io.ReaderAt, size int64) (*Reader, error)
 
 // Writer implements a zip file writer.
 
-// Writer类型实现了zip文件的写入器。
+// Writer 类型实现了 zip 文件的写入器.
 type Writer struct {
 	// contains filtered or unexported fields
 }
 
 // NewWriter returns a new Writer writing a zip file to w.
 
-// NewWriter创建并返回一个将zip文件写入w的*Writer。
+// NewWriter 创建并返回一个将 zip 文件写入 w 的 Writer.
 func NewWriter(w io.Writer) *Writer
 
 // Close finishes writing the zip file by writing the central directory. It does
 // not (and can not) close the underlying writer.
 
-// Close方法通过写入中央目录关闭该*Writer。
-// 本方法不会也没办法关闭下层的io.Writer接口。
+// Close 方法通过写入中央目录来关闭该 Writer. 本方法不会也没办法关闭下层的 io.Writer 接口.
 func (w *Writer) Close() error
 
 // Create adds a file to the zip file using the provided name. It returns a Writer
@@ -210,10 +225,9 @@ func (w *Writer) Close() error
 // forward slashes are allowed. The file's contents must be written to the
 // io.Writer before the next call to Create, CreateHeader, or Close.
 
-// 使用给出的文件名添加一个文件进zip文件。
-// 本方法返回一个io.Writer接口（用于写入新添加文件的内容）。
-// 文件名必须是相对路径，不能以设备或斜杠开始，只接受'/'作为路径分隔。
-// 新增文件的内容必须在下一次调用CreateHeader、Create或Close方法之前全部写入。
+// Create 使用给定的文件名添加一个文件进 zip 文件. 本方法返回一个 io.Writer 接口(用于写入
+// 新文件的内容). 文件名必须是相对路径, 不能以设备或斜杠开始, 只接受'/'作为路径
+// 分隔. 新文件的内容必须在下一次调用 CreateHeader、Create 或 Close 方法之前全部写入.
 func (w *Writer) Create(name string) (io.Writer, error)
 
 // CreateHeader adds a file to the zip file using the provided FileHeader for the
@@ -221,11 +235,13 @@ func (w *Writer) Create(name string) (io.Writer, error)
 // The file's contents must be written to the io.Writer before the next call to
 // Create, CreateHeader, or Close.
 
-// 使用给出的*FileHeader来作为文件的元数据添加一个文件进zip文件。
-// 本方法返回一个io.Writer接口（用于写入新添加文件的内容）。
-// 新增文件的内容必须在下一次调用CreateHeader、Create或Close方法之前全部写入。
+// CreateHeader 使用给定的 FileHeader 来作为文件的元数据添加一个文件进 zip 文件.
+// 本方法返回一个 io.Writer 接口(用于写入新文件的内容). 新文件的内容必须在下一次调
+// 用 CreateHeader、Create 或 Close 方法之前全部写入.
 func (w *Writer) CreateHeader(fh *FileHeader) (io.Writer, error)
 
 // Flush flushes any buffered data to the underlying writer. Calling Flush is not
 // normally necessary; calling Close is sufficient.
+
+// Flush 将缓冲中的数据刷新到下层 Writer 中. 调用 Flush 不是必要的; 调用 Close 就足够了.
 func (w *Writer) Flush() error
